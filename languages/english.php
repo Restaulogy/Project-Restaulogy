2a504b3649798b3d6dac3f4acaac87db7c3a27ec
sangramkokate <inforesha.sangram@gmail.com>
2018-06-07 12:30:26 +0530

    BASE CODE-COMMIT-2
    BASE CODE-COMMIT-2..Since we cannot upload all files at a time step by step download

---
+++ languages/english.php
@@ -0,0 +1,175 @@
+<?php  
+	/* This language file containes all the words & phrases used in the application. They are in english language. */
+
+	/* $_lang for the home page. All language variables for the home page are below. */
+	$_lang['home'] = 'Home';
+	$_lang['home_promo'] = 'A simple yet powerful authentication & user management system which enable webmasters to restrict any part(s) of their website to registered and logged in users only. It is power packed with some amazing features and backend options. It is said to be a developer friendly management system.';
+	$_lang['to_protect'] = 'To protect a page using auth manager just use the following code:';
+	$_lang['logged_in'] = 'Welcome! %s.';
+	$_lang['lbl_manage'] = 'Manage';
+	$_lang['lbl_back'] = 'Back to';
+	
+	/* $_lang for the footer values. Replace the following with your own links. We request 
+	you to keep the copyright notice intact. Give us some credits for this product. */
+	$_lang['contact_us'] = 'Contact Us';
+	$_lang['freebies'] = 'Freebies';
+	
+	/* $_lang for the registration page are listed below. */
+	$_lang['register'] = 'Register';
+	$_lang['register_at'] = 'Register at';
+	$_lang['join_now'] = 'Join Now';
+	$_lang['username_notice'] = '(This will be your username)'; 
+	$_lang['sign_in'] = 'Sign In';
+	$_lang['sign_up'] = 'Sign Up';
+	$_lang['sign_out'] = 'Sign out';
+	
+	/* $_lang for common variables on different pages. */
+	$_lang['email'] = 'Email';
+	$_lang['password'] = 'Password';
+	$_lang['reset_password'] = 'Reset Password';
+	$_lang['verify_login'] = 'Verify login details';
+	$_lang['resend_confirmation'] = 'Resend confirmation email';
+	$_lang['forgot_password'] = 'Forgot Password';
+	$_lang['verify'] = 'Verify';
+	$_lang['remember_me'] = 'Remember Me';
+	$_lang['already_on'] = 'Already on';
+	$_lang['login'] = 'Login';
+	$_lang['email_verification'] = 'Email Verification';
+	$_lang['enter_the_email'] = '(Enter the email you used at the time of registration)';
+	$_lang['dont_use_public'] = '(Don`t use on public computers)';
+	$_lang['password_generation'] = 'Generate Password';
+	$_lang['protected_page'] = 'Protected Page';
+	$_lang['unprotected_page'] = 'Un-protected Page';
+	
+	$_lang['update_profile'] = 'Edit Profile';
+
+	/* $_lang for the header page are listed below. */
+	$_lang['welcome'] = 'Welcome';
+	$_lang['logout'] = 'Logout'; 
+	$_lang['please_wait'] = 'Processing';
+
+	/* $_lang for the login page are listed below. */
+	$_lang['click_here'] = 'Click here';
+	$_lang['recover_your_password'] = 'to recover your password';
+	$_lang['to_login'] = 'to login to the website';
+	
+	/* $_lang for the errors on all the pages. */
+	$_lang['account_banned'] = 'This account is temporarily deleted.  If you would like to restore please contact the manager.Sorry for the inconvenience caused.';
+	$_lang['new_password_sent'] = 'Your new password has been sent to your email.';
+	$_lang['email_not_found'] = 'The email or phone number entered is not registered in our loyalty program.';
+	$_lang['verification_error'] = 'Please enter correct words in the verification box.';
+	$_lang['empty_fields'] = 'Please fill in all the fields.';
+	$_lang['already_logged_in'] = 'This page cannot be accessed by a logged in user.';
+	$_lang['active_session'] = 'You have an active session.';
+	$_lang['invalid_email'] = 'The email does not seem to be valid.';
+	$_lang['email_exists'] = 'This email is already registered with us.';
+	$_lang['phone_exists'] = 'This phone number is already registered with us.';	
+	$_lang['resend_confirmation_link'] = 'Click here to resend confirmation email';
+	$_lang['unable_to_register'] = 'Unable to register you at this time.';
+	$_lang['try_again_later'] = 'Please try again later.';
+	$_lang['verify_your_email'] = 'Please verify your email by clicking the confirmation link sent to your mail.';
+	$_lang['registration_success'] = 'Registration completed. You are now a member of %s.';
+	$_lang['start_now'] = 'Start using the website instantly by <a href="'.$website.'/user/login.php">click here</a>.';
+	$_lang['account_active'] = "You already have an active account with us.";
+	$_lang['confirmation_email_sent'] = 'Your confirmation email has been sent.';
+	$_lang['verification_invalid'] = 'Your verification key is not a valid one.';
+	$_lang['invalid_link'] = 'It seems that you have followed an invalid link. Make sure that the link you followed is not ages old and the source is trustable.';
+	$_lang['contact_admin'] = '';//'Please contact site admin for any further assistance.';
+	$_lang['email_verified'] = 'Your email has been verified.';
+	$_lang['email_not_verified'] = 'You have not verified your email yet.';
+	$_lang['id_pass_mismatch'] = 'Invalid username & password combination.';
+	$_lang['mob_device_mismatch'] = 'Device id not matched.';
+	$_lang['mob_device_already'] = 'Device id is already exists.';
+	$_lang['login_error'] = 'Problem occured during login.';
+	$_lang['attempts_exceeded'] = 'Login attempts exceeded. Please verify you are human.';
+	$_lang['password_reset_link'] = 'An email has been sent to %s with instructions for resetting your password. This password reset is only valid for 4 hours after which it will be cancelled and your password will remain unchanged. If the email does not arrive in the next few minutes, please check your spam filters.';
+	$_lang['reset_not_found'] = 'The password reset key is not a valid one.';
+	$_lang['generate_link'] = 'Use the <a href="'.$website.'/user/forgot.php">forgot password</a> page to generate a new password reset key.';
+	$_lang['time_expired'] = 'The password reset link has expired. You can only use the link within 24 hours of requesting it.';
+	$_lang['global_error'] = 'Oops!';
+	$_lang['must_login'] = 'You must login to view this.';
+	$_lang['cancel_lbl'] = 'Cancel';
+	 
+	$_lang['preview_lbl'] = 'Preview';
+	$_lang['close_lbl'] = 'Close'; 
+	$_lang['save_lbl'] = 'Save'; 
+	$_lang['delete_lbl'] = 'Delete'; 
+	$_lang['back_lbl'] = 'Back';
+	$_lang['update_lbl'] = 'Update'; 
+	$_lang['create_lbl'] = 'Create'; 
+	$_lang['search_lbl'] = 'Search';  
+	$_lang['field_title'] = 'Fields';
+	$_lang['value_title'] = 'Values';
+	
+
+	$_lang['friendlytime:justnow'] =  'just now';
+	$_lang['friendlytime:minutes'] = '%s minutes ago';
+	$_lang['friendlytime:minutes:singular'] = 'a minute ago';
+	$_lang['friendlytime:hours'] = '%s hours ago';
+	$_lang['friendlytime:hours:singular'] = 'an hour ago';
+	$_lang['friendlytime:days'] = '%s days ago';
+	$_lang['friendlytime:days:singular'] = 'yesterday';		
+	$_lang['friendlytime:year'] = 'an year ago';
+	$_lang['friendlytime:longdays'] = 'long days ago';
+	
+	$_lang['select_states'] = 'Select States';
+	$_lang['select_metro_area'] = 'Select Metro Area';
+	$_lang['select_city'] = 'Select city';
+	$_lang['ADD_TO_CART'] = 'Add to Order';
+	$_lang['UPDATE_TO_CART'] = 'Update Order';
+	$_lang['REMOVE_FROM_CART'] = 'Remove from Order';
+	
+	$_lang['cancel_dish_order_confirmation'] = 'Are you sure to cancel this dish?';
+	
+	$_lang['by_label'] = 'By';
+	$_lang['on_label'] = 'On';
+	$_lang['no_thanks_lbl'] = 'No Thanks';
+	$_lang['live'] = 'Live';
+	$_lang['expired'] = 'Past';
+	$_lang['completed'] = 'Completed';
+	$_lang['manage_str'] = 'Manage %s';
+	$_lang['back_str'] = 'Back To %s';
+	$_lang['posted'] = 'Posted';
+	$_lang['claim_coupon'] = 'Claimed Promotion';
+	$_lang['lbl_coupon']	= 'Promotion';
+	$_lang['lbl_coupons']	= 'Promotions';
+	$_lang['lbl_prom_terms'] = 'Terms & Condition';
+	$_lang['table_unavailable'] = 'If you are getting this message when you scan the QR code assigned to your table, please notify the staff.'; 
+	$_lang['schedule_unavailable'] = 'There are currently no schedules availabel for today.Please set it first.';
+	$_lang['emp_cannot_login_this_time'] = 'You can login only within your shift timing.';
+	$_lang['emp_not_within_distance'] = 'You are not within range of restaurant location.';
+	/*$_lang['claim_success'] = 'Promotion claimed successfully.You can see it in order details and you need to apply it.';*/
+	$_lang['claim_success'] = 'Promotion claimed successfully.';
+	
+	$_lang['mandotory_mark']='&nbsp;(<span style="color:red;">*</span>)';
+	$_lang['empty_text_mark'] = '--';
+	$_lang['denote_mark'] = ':';
+	$_lang['percent_mark'] = '%';
+	$_lang['currency_mark'] = '$';
+	
+	$_lang['approve'] = 'Approve';
+	$_lang['reject'] = 'Reject';
+	
+	$_lang['_details'] = 'Details';
+	//..for downtown butcher demo messages
+	$_lang['downtown_demo_social_lnk'] = 'This is demo once we go live it will redirect you to our social media site';
+	$_lang['downtown_demo_email_friend_lnk'] = 'This is demo once we go live you will be able your friend using this.';
+	$_lang['downtown_demo_payment_lnk'] = 'The payment module will be integrated once we go live.';
+	$_lang['downtown_demo_review_lnk'] = 'This is demo once we go live it will redirect you to review site';
+	
+	/* File ends over here */
+	
+	/* Custom Change starts here */ 
+	
+	//... Added the custom lang files  
+	$LANG_DIRECTRORY = dirname(__FILE__).'/english/'; 
+	$ini_FILES = glob($LANG_DIRECTRORY.'*.ini'); 
+	 foreach($ini_FILES as $ini_FILE){
+	    $file_name = $ini_FILE;
+	    $file_name = str_replace( $LANG_DIRECTRORY, '' , $ini_FILE);
+		$file_name = str_replace( '.ini' , '', $file_name);
+
+	 	$_lang[$file_name] = parse_ini_file($ini_FILE , true); 
+	}
+	 
+?>
